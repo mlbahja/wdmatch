071451d0ef88050d3dabd0764dae9c8c2869d6eb
mlbahja <lbahjamustapha990@gmail.com>
2024-02-05 16:35:21 +0100

    first

---
+++ wdmatch.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printstr(s string) {
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		return
+	}
+	args1 := os.Args[1]
+	s1 := []rune(args1)
+	args2 := os.Args[2]
+	s2 := []rune(args2)
+
+	j := 0
+	result := ""
+	for i := 0; i < len(s2); i++ {
+		if j < len(s1) && s1[j] == s2[i] {
+			result += string(s1[j])
+			j++
+		}
+	}
+	if result == string(s1) {
+		printstr(result)
+	}
+}
